cd30e792f1535db9964e10a30ffc233838631c91
iCoderLad <iCoderLad@163.com>
2022-09-26 19:37:10 +0800

    fix

--- src/test/java/com/example/demo/Solution.java
+++ src/test/java/com/example/demo/Solution.java
@@ -1,23 +1,10 @@
 package com.example.demo;
 
 
-import java.util.HashSet;
-import java.util.Random;
-import java.util.Set;
-
-public class Solution {
-
+public class Solution{
     public static void main(String[] args) {
-        Set<Integer> set = new HashSet<>();
-        for (int i = 0; i < 5; i ++){
-            set.add(new Random().nextInt(5));
-        }
-        System.out.println(set);
-        for (Integer integer : set) {
-            System.out.println(integer);
-        }
+
 
     }
-
 
 }
